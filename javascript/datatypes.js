076bc3d33f64e73fc2f45818484cfdc8bc638e66
Sasi2B3 <sasikumar88868@gmail.com>
2024-07-06 15:41:56 +0530

    project_1byusingjavascript

---
+++ javascript/datatypes.js
@@ -0,0 +1,30 @@
+let variblel;
+variablel=3;
+console.log(variablel);
+
+var variable2=4;
+console.log(variable2);
+
+const var3="hello";
+console.log(var3);
+
+//datatypes
+let a=4;
+var name="deepu";
+const isit=true;
+var array=(a,name,isit);
+let obj={name:"deepu"}
+console.log(typeof a,typeof name,typeof isit)
+console.log(typeof array,typeof obj)
+
+//if else
+function add(a){
+    console.log(a);
+}
+let a=4;
+if(a%2===0){
+    console.log(even)
+}
+else{
+    console.log(odd)
+}
